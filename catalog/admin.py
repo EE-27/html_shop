0af85bf3153016e815d989493469838c1283279d
JL34 <johnny2791@seznam.cz>
2023-11-24 13:37:26 +0100

    DÚ - 2  Task 5 For the category and product models, configure the display in the administration panel. For categories, list the id and name in the display list, and for products, list the id, name, price and category.
    Set up the product display interface so that you can filter the display results by category and search by name and description field.

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+
+# py manage.py createsuperuser
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price_per_piece', 'category',)
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description',)
+    search_fields = ('name', 'description',)
--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,48 @@
 from django.db import models
 
 # Create your models here.
+"""
+Task 2 In the catalogue application, create models:
+
+Product, Category.
+
+Describe the initial settings for them.
+
+Task 3 For each model, describe the following fields:
+
+Product: name, description, image (preview), category, price per piece, creation date,
+ last modified date. Category: name, description.
+"""
 
 
+# pip install Pillow
+# py manage.py makemigrations - také update
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name")
+    description = models.CharField(max_length=255, verbose_name="Description")
+    image = models.ImageField(verbose_name="Image", null=True, blank=True)
+    category = models.CharField(max_length=100, verbose_name="Category")
+    price_per_piece = models.IntegerField(verbose_name="Price per Piece")
+    creation_date = models.DateField(verbose_name="Creation Date")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        ordering = ("name",)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Name")
+    description = models.CharField(max_length=255, verbose_name="Description")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ("name",)
